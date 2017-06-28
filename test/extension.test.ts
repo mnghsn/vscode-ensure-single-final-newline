1e9c925b67b0a59efb1e5651af151348f156f4a8
Jimmy Lin <jmlntw@gmail.com>
2017-06-28 16:05:04 +0800

    Increase all mocha timeout value

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -14,30 +14,30 @@ suite('Ensure Single Final Newline', () => {
     test('no newline; ensureSingleFinalNewline = true; insertFinalNewline = false;', async () => {
         const document = await create(true, false);
         assert.strictEqual(await create(true, false).saveText('foo'), `foo${EOL}`);
-    });
+    }).timeout(5000);
 
     test('no newline; ensureSingleFinalNewline = false; insertFinalNewline = true;', async () => {
         assert.strictEqual(await create(false, true).saveText('foo'), `foo${EOL}`);
-    });
+    }).timeout(5000);
 
     test('no newline; ensureSingleFinalNewline = false; insertFinalNewline = false;', async () => {
         assert.strictEqual(await create(false, false).saveText('foo'), 'foo');
-    });
+    }).timeout(5000);
 
     test('2 newlines; ensureSingleFinalNewline = true; insertFinalNewline = true;', async () => {
         assert.strictEqual(await create(true, true).saveText(`foo${EOL}${EOL}`), `foo${EOL}`);
-    });
+    }).timeout(5000);
 
     test('2 newlines; ensureSingleFinalNewline = true; insertFinalNewline = false;', async () => {
         assert.strictEqual(await create(true, false).saveText(`foo${EOL}${EOL}`), `foo${EOL}`);
-    });
+    }).timeout(5000);
 
     test('2 newlines; ensureSingleFinalNewline = false; insertFinalNewline = true;', async () => {
         assert.strictEqual(await create(false, true).saveText(`foo${EOL}${EOL}`), `foo${EOL}${EOL}`);
-    });
+    }).timeout(5000);
 
     test('2 newlines; ensureSingleFinalNewline = false; insertFinalNewline = false;', async () => {
         assert.strictEqual(await create(false, false).saveText(`foo${EOL}${EOL}`), `foo${EOL}${EOL}`);
-    });
+    }).timeout(5000);
 
 });
